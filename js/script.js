65979a328da59eb721cb736c746b8f027d175508
Myroslav Hanets <miriskofficial@gmail.com>
2024-02-15 15:07:01 +0200

    asterix

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
-let numberOfFilms = prompt('Скільки фільмів ви подивилися?', '');
+// let numberOfFilms = prompt('Скільки фільмів ви подивилися?', '');
 
 
-let PersonalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
+// let PersonalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false
+// };
 
-PersonalMovieDB.movies[prompt('Котрий з останніх фільмів ви подивилися?', '')] = prompt('Як би ви його оцінили?', '');
+// PersonalMovieDB.movies[prompt('Котрий з останніх фільмів ви подивилися?', '')] = prompt('Як би ви його оцінили?', '');
 
-console.log(PersonalMovieDB);
+// console.log(PersonalMovieDB);
+
+let result = ''
+for (let i = 1; i <= 6; i++) {
+  result += '*'
+  console.log(result)
+}
