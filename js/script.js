2779c18d67dc8834c1b20614af5cd445df4ba535
Myroslav Hanets <miriskofficial@gmail.com>
2024-02-15 12:55:22 +0200

    1.0

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log(1);
+
+const storeName = 'GloriaMix';
+const storeDescription = {
+	budget: 10000,
+	employees: ['Derek', 'Daimon', 'Richard'],
+	products: {
+		orange: 500,
+		elda: 10
+	},
+	open: true
+};
+
+console.log(storeDescription);
