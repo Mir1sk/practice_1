26d0fec74c00dfae1375775f31c59f628a5faef8
Myroslav Hanets <miriskofficial@gmail.com>
2024-02-15 20:00:21 +0200

    NumberOfFilms 1.2

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
-// let numberOfFilms = prompt('Скільки фільмів ви подивилися?', '');
+let numberOfFilms = prompt('Hello скільки фільмів ви переглянули?', '');
 
-
-// let PersonalMovieDB = {
-// 	count: numberOfFilms,
-// 	movies: {},
-// 	actors: {},
-// 	genres: [],
-// 	privat: false
-// };
-
-// PersonalMovieDB.movies[prompt('Котрий з останніх фільмів ви подивилися?', '')] = prompt('Як би ви його оцінили?', '');
-
-// console.log(PersonalMovieDB);
-
-let result = ''
-for (let i = 1; i <= 6; i++) {
-  result += '*'
-  console.log(result)
+let PersonalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
 }
+
+
+// first try
+for (let i = 1; i < 2; i++) {
+  const film = prompt('Котрий був з останніх фільмів?', ''),
+        rating = prompt('Як би ви його оцінили?', '');
+
+  if (film != null && rating != null && film != '' && rating != '' && film.length < 50) {
+    PersonalMovieDB.movies[film] = rating;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (PersonalMovieDB.count <= 10) {
+  alert('Переглянуто доволі мало фільмів')
+} else if (PersonalMovieDB.count <= 30) {
+  alert('Ви класичний глядач фільмів')
+} else if (PersonalMovieDB.count <= 5000) {
+  alert('Ви кіноман')
+} else if (PersonalMovieDB.count > 5000) {
+  alert('П*здабол, щось не вірю вам')
+} else {alert('Трапилася помилка')}
+
+console.log(PersonalMovieDB);
+
